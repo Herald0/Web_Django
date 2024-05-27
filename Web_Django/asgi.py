6c7ecac4197c65b5967ca3a04a7fb8152785bc66
Herald <tiuopall@gmail.com>
2024-05-27 22:20:41 +0300

    4 update

--- Web_Django/asgi.py
+++ Web_Django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Web_project project.
+ASGI config for Web_Django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web_Django.settings')
 
 application = get_asgi_application()
--- Web_Django/wsgi.py
+++ Web_Django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Web_project project.
+WSGI config for Web_Django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web_Django.settings')
 
 application = get_wsgi_application()
